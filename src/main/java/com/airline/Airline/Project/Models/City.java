e42b8aa01d720d021ebdbaeb1d3a065c785f6f8f
Jacob Nottoli <jacob.nottoli@gmail.com>
2022-11-08 14:15:40 -0600

    City model created

---
+++ src/main/java/com/airline/Airline/Project/Models/City.java
@@ -0,0 +1,21 @@
+package com.airline.Airline.Project.Models;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Cities")
+@Builder
+@ToString
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int city_id;
+    @Column
+    private String name;
+}
