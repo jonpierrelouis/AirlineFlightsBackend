eeee166df097e574de0c1e5b8f85f2d52712deaf
Jacob Nottoli <jacob.nottoli@gmail.com>
2022-11-08 13:46:00 -0600

    first commit

---
+++ src/test/java/com/airline/Airline/Project/AirlineProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airline.Airline.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
