c425a4df909ad80184b914de5c6ed5a4f39142a9
jonpierrelouis <47282759+jonpierrelouis@users.noreply.github.com>
2022-11-09 15:11:38 -0500

    Update CityController.java

--- src/main/java/com/airline/Airline/Project/Controller/CityController.java
+++ src/main/java/com/airline/Airline/Project/Controller/CityController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin()
+@CrossOrigin(origins ="*")
 public class CityController {
     private final CityService cs;
 
